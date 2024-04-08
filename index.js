7e5c95cfae7f9b1a0a561ce0e752427dd0a397c7
biizakkyzyazem <azemeshimova@gmail.com>
2024-04-08 10:43:59 +0600

    clean

---
+++ index.js
@@ -0,0 +1,75 @@
+// const a = {
+//     num: 23,
+//     str: 'Hello',
+// };
+
+// const b = a;
+// const c = {}
+
+// consol.log('a: ', a);
+// console.log('b🐎: ', b);
+// console.log('a === b', a === b);
+
+// for (let key in a) {
+//     c[key] = a[key];
+// }
+
+// c.isTrue = true;
+
+// const f = {
+//     a: 'a',
+//     b: 3738449,
+//     d: 'DOD',
+//     getA: function () {
+//         console.log(this.a);
+//     },
+// };
+
+// console.log('f🐅: ', f);
+// console.log('c 🦚: ', c);
+// console.log('a === c', a === c);//false
+
+
+// const d = Object.assign({}, a, f, c);
+// console.log('Object.assign(d, a)🦍; ', Object.assign(d, a));
+// d.isTrue = true;
+// // console.log('d🐆:', d);
+// console.log('a === d 🦩:', a === d); //false 
+
+// const obj = {
+//     name: 'Name',
+//     getThis: function () {
+//         console.log('this', this);
+
+//         return this;
+//     },
+//     number: 1234567,
+//     boolenValue: false,
+// };
+
+// console.log('obj.getThis', obj.getThis());
+
+
+
+// function Student(name, lastname) {
+//     this.fristname = name;
+//     this.lastname = lastname;
+//     console.log(this);
+
+//     return this;
+// };
+
+
+// const student1 = new Student('janara', 'Tolgonbaeva');
+// console.log('student1', stydent1);
+
+
+// const student2 = new Student('Azem', 'Biizak kyzy');
+// student2.sayHello = function () {
+//     console.log('I say hello');
+//     return 'Hello';
+// };
+// console.log('student2', stydent2, stydent2.sayHello());
+
+
+
